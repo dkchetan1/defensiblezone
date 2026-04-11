4a44c8fc3a3f0aeed8ef39e84865912056e9570f
dkchetan1 <dilip.chetan@gmail.com>
2026-04-10 20:48:28 -0700

    Add designer route and gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Landing from "./Landing.jsx";
 import Engineer from "./Engineer.jsx";
 import Doctor from "./Doctor.jsx";
+import Designer from "./Designer";
 import About from "./About.jsx";
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
 
   if (path === "/doctor" || path === "/doctor/") return <Doctor />;
   if (path === "/engineer" || path === "/engineer/") return <Engineer />;
+  if (path === "/designer" || path === "/designer/") return <Designer />;
   if (path === "/about" || path === "/about/") return <About />;
   return <Landing />;
 }
